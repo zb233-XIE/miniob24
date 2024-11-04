959e393b5fe0067ba58a4467eadbd7d6a8b3e485
avalanche <anwentao1@gmail.com>
2024-11-04 18:28:17 +0800

    fix: if table's alias is empty, skip inserting into table_map

--- src/observer/sql/stmt/select_stmt.cpp
+++ src/observer/sql/stmt/select_stmt.cpp
@@ -82,7 +82,7 @@ RC SelectStmt::create(Db *db, SelectSqlNode &select_sql, Stmt *&stmt)
       return RC::SCHEMA_TABLE_NOT_EXIST;
     }
 
-    if (table_map.count(select_sql.relations[i].alias) != 0) {
+    if (!select_sql.relations[i].alias.empty() && table_map.count(select_sql.relations[i].alias) != 0) {
       LOG_WARN("duplicate table alias. alias=%s", select_sql.relations[i].alias.c_str());
       return RC::TABLE_ALIAS_DUPLICATE;
     }
@@ -90,7 +90,9 @@ RC SelectStmt::create(Db *db, SelectSqlNode &select_sql, Stmt *&stmt)
     binder_context.add_table(table);
     tables.push_back(table);
     table_map.insert({table_name, table});
-    table_map.insert({select_sql.relations[i].alias, table});
+    if (!select_sql.relations[i].alias.empty()) {
+      table_map.insert({select_sql.relations[i].alias, table});
+    }
   }
 
   // 将join语句中的表也加入到tables和table_map中，依葫芦画瓢
